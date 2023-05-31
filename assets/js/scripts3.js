4694b5693300df57b2fba225844cf3d06fea2b4f
Aldodo <45825599+Aldodo91@users.noreply.github.com>
2023-05-31 20:25:43 +0200

    Wine Bar

---
+++ assets/js/scripts3.js
@@ -0,0 +1,75 @@
+const makeItStrong = (p) => {
+  const firstSlashIndex = p.indexOf("/");
+  const lastSlashIndex = p.lastIndexOf("/");
+  if (firstSlashIndex <= 0) {
+    return `${p}`;
+  }
+  const textBetweenSlashes = p.substring(
+    firstSlashIndex + 1,
+    p.indexOf("/", firstSlashIndex + 1)
+  );
+  const textBeforeFirstSlash = p.substring(0, firstSlashIndex);
+  console.log(textBeforeFirstSlash, textBetweenSlashes);
+  const restOfString = p.substring(lastSlashIndex + 1, p.length);
+
+  return `<strong>${textBeforeFirstSlash}</strong> <i>${textBetweenSlashes}</i> ${restOfString}`;
+};
+setTimeout(() => {
+  var xhttp = new XMLHttpRequest();
+  var tbody = document.getElementById("menuBar");
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const categories = [
+        "Vini Rossi",
+        "Vini Bianchi",
+        "Vini Rose",
+        "Bollicine",
+        "Bar & Aperitivi",
+        "Vini a Calice",
+        "Taglieri",
+      ];
+      const words = xhttp.responseText;
+
+      words.split(/\r?\n/).map((row) => {
+        var tr = document.createElement("tr");
+        var tdm = document.createElement("td");
+        var tdp = document.createElement("td");
+        if (row === "") return;
+
+        row.split("€").map((p, i) => {
+          if (categories.includes(p)) {
+            var trh = document.createElement("tr");
+            var thh1 = document.createElement("th");
+            var thh2 = document.createElement("th");
+
+            thh1.textContent = p;
+            thh1.classList.add("w80");
+            thh2.textContent = "Prezzo";
+            thh1.classList.add("lineogg");
+            thh2.className += "linepre";
+            trh.appendChild(thh1);
+            trh.appendChild(thh2);
+            tbody.appendChild(trh);
+            return;
+          } else if (!categories.includes(p) && i == 0) {
+            // voce di menu
+            tdm.innerHTML = makeItStrong(p);
+          } else if (!categories.includes(p) && i == 1) {
+            // voce di costo
+            tdp.style.textAlign = "center";
+            tdp.textContent = `€ ${p}`;
+          }
+
+          if (tdm !== "") {
+            tr.appendChild(tdm);
+            tr.appendChild(tdp);
+            tbody.appendChild(tr);
+          }
+        });
+      });
+    }
+  };
+  xhttp.open("GET", "./Bar.txt", true);
+  xhttp.send();
+}, 0);
